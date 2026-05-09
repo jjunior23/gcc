8fee72103909036a07a02ee971bd41f47ddd6025
José Gonzaga <josegonzaga23@gmail.com>
2026-05-09 19:43:09 -0300

    segundo commit

--- core/views.py
+++ core/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-    return HttpResponse('<html><title>gcc</title></html>')
+    return HttpResponse('<html><title>gcc</title>teste</html>')
 
