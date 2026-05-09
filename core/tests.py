63dd0eeb431b1f2c2c12e107bde04122f053d27e
José Gonzaga <josegonzaga23@gmail.com>
2026-05-09 17:03:29 -0300

    teste commit

--- core/tests.py
+++ core/tests.py
@@ -1,11 +1,20 @@
 from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest  # (1)
+
 from core.views import home_page
 
-# Create your tests here.
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()  # (1)
+        response = home_page(request)  # (2)
+        html = response.content.decode("utf8")  # (3)
+        self.assertIn("<title>gcc</title>", html)  # (4)
+        self.assertTrue(html.startswith("<html>"))  # (5)
+        self.assertTrue(html.endswith("</html>"))  # (5)
